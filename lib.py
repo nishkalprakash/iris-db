f03f270db03b7061ad0fd66668210e36f4d39b53
N P <nishkalprakash@gmail.com>
2025-08-28 14:38:43 +0530

    made a bare min IRIS_DB class with capabilities to list_db and auto close connections

--- lib.py
+++ lib.py
@@ -1,71 +1,76 @@
-## This file has other functions that may be helpful like print wrapper and a global parallel processor
-from multiprocessing import Pool
-from os import cpu_count
-from time import strftime
+#%% init
+# from json import load, dumps
+from pathlib import Path
 
-def dprint(func):
-    def wrapped_func(*args, **kwargs):
-        return func(strftime("%H:%M:%S - "),*args,**kwargs)
-    return wrapped_func
+## Logger setup
+import logging
+pymongo_logger = logging.getLogger('pymongo')
+# Set its level to WARNING to silence INFO and DEBUG messages
+pymongo_logger.setLevel(logging.WARNING)
+class LoggerManager:
+    """Singleton Logger Manager for consistent logging across the app."""
+    _logger = None
 
-print = dprint(print)
-# class Batcher:
-#     def __init__(self,
-#                     folder='/home/rs/19CS91R05/DarKSkuLL/BI/Anguli_200k_1M',
-#                     ext='*.tiff',
-#                     batch_size=1000
-#                 ) -> iter:
-#         self.folder=folder
-#         self.ext=ext
-#         self.batch_size=1000
-#         self.proc_count=cpu_count()-1
-#         self.batch = iter()
+    @classmethod
+    def get_logger(cls, log_dir="logs/", name=__name__, level="DEBUG"):
+        if cls._logger is None:
+            log_dir = Path(log_dir)
+            log_dir.mkdir(exist_ok=True)
+            log_file = log_dir / f"{name}.log"
+            logging.basicConfig(
+                level=logging.DEBUG,
+                # also log the calling function name
+                format='%(asctime)s - %(name)s - %(funcName)s - %(levelname)s - %(message)s',
+                # datefmt='%Y-%m-%d %H:%M:%S',
+                handlers=[
+                    logging.FileHandler(log_file, encoding="utf-8"),
+                    logging.StreamHandler()
+                ]
+            )
+            # set logging level
+            if isinstance(level, str):
+                level = getattr(logging, level.upper(), logging.INFO)
+            cls._logger = logging.getLogger(name)
+            cls._logger.setLevel(level)
+        return cls._logger
 
-#     # def
-#     def __call__(self, ) -> iter:
-#         return next(self.batch)
-class Parallel:
-    def __init__(self,debug=False) -> object:
-        self.debug=debug
-        print("Parallel Instance Created")
+lg = LoggerManager.get_logger(__name__)
+
+class FileManager:
+    """Singleton File Manager for consistent file operations."""
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(FileManager, cls).__new__(cls)
+        return cls._instance
+
+    # @staticmethod
+    # def load_json_file(filename):
+    #     """Load and return data from a JSON file."""
+    #     with open(filename, 'r') as file:
+    #         return load(file)
+
+    # @staticmethod
+    # def write_json_file(filename, data):
+    #     """Write data to a JSON file."""
+    #     with open(filename, 'w') as file:
+    #         file.write(dumps(data, indent=4, ensure_ascii=False))
     
-    def batcher(self,doc):
-        if doc:
-            self.doc_list.append(doc)
-            # self.ctr += 1
-            self.done += 1
-            if len(self.doc_list) >= self.batch_size:
-                # self.ctr = 0
-                print(f"{self.done}/{len(self.paths)} documents processed")
-                # if len(self.doc_list):
-                dc=self.doc_list[:]
-                self.doc_list = []
-                return dc
-            return None
-            # if len(self.doc_list):
-            #     dc=self.doc_list[:]
-            #     self.doc_list = []
-            #     return dc
+    @staticmethod
+    def read_creds(filename="mongo_creds.txt"):
+        """Read MongoDB credentials from a file using pathlib"""
+        if not Path(filename).exists():
+            _ERROR_MSG = f"Credentials file {filename} does not exist.Please create a mongo_creds.txt with user:passwd"
+            lg.error(_ERROR_MSG)
+            raise FileNotFoundError(_ERROR_MSG)
+        user, passwd = Path(filename).read_text().strip().split(":")
+        lg.debug("MongoDB credentials loaded successfully.")
+        return user, passwd
 
-    def __call__(
-        self, atomic_function, paths, batch_size=100, chunksize=10, free_core=1
-    ) -> dict:
-        self.paths=paths
-        self.batch_size=batch_size
-        self.ctr = self.done = 0
-        self.doc_list = []
-        if not self.debug:
-            with Pool(cpu_count() - free_core - 1) as p:
-                for doc in p.imap(atomic_function, paths, chunksize):
-                    if (dc:=self.batcher(doc)):
-                        yield dc
-                if len(dc:=self.doc_list[:]):
-                    self.doc_list = []
-                    yield dc
-        else:
-            for path in paths:
-                if (dc:=self.batcher(atomic_function(path))):
-                    yield dc
-            if len(dc:=self.doc_list[:]):
-                self.doc_list = []
-                yield dc
+
+#%% Testing
+# lg.WARNING
+# import LoggerManager as lm
+# logging.WARNING
+# %%
