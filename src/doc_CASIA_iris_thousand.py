3a9093e639f122629d299920fceb92d2a51ff1db
N P <nishkalprakash@gmail.com>
2025-09-16 20:20:29 +0530

    done for casia iris thousand

---
+++ src/doc_CASIA_iris_thousand.py
@@ -0,0 +1,31 @@
+doc = {
+    "_id" : "68b837aaca5135cbc046ef40",
+    "ds_id" : "CASIA_iris_thousand",
+    "person_id" : "1000",
+    "eye_id" : "1000_L",
+    "sample_id" : "10",
+    "person_sample_id" : "10",
+    "image_id" : "1000_L_10",
+    "file_name" : "1000_L_10.jpg",
+    "session_id" : "1",
+    "eye" : "L",
+    "img_specs" : {
+        "ext" : ".jpg",
+        "res" : "640x480",
+        "width" : 640,
+        "height" : 480
+    },
+    "paths" : {
+        "base_" : "/home/nishkal/datasets/iris_db/CASIA_iris_thousand",
+        "rel_path_" : "1000_L/1000_L_10.jpg",
+        "path_" : "/home/nishkal/datasets/iris_db/CASIA_iris_thousand/orig/1000_L/1000_L_10.jpg"
+    },
+    "orig_paths" : {
+        "base_" : "/home/nishkal/datasets/iris_datasets/CASIA/V4/CASIA-IrisV4(JPG)/CASIA-Iris-Thousand",
+        "rel_path_" : "000/L/S5000L00.jpg",
+        "path_" : "/home/nishkal/datasets/iris_datasets/CASIA/V4/CASIA-IrisV4(JPG)/CASIA-Iris-Thousand/000/L/S5000L00.jpg"
+    },
+    "img_tags" : [
+        "orig"
+    ]
+}
